3fdb671da374ef40e94ec71c7a3fea56e2bb08f5
VegettoSanDev <vegetto.san1178@gmail.com>
2026-08-09 18:32:54 -0500

    0

--- Client PSVitaAlive/build/CMakeFiles/PSVitaAlive-velf.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/PSVitaAlive-velf"
-  "PSVitaAlive.velf"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/PSVitaAlive-velf.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
