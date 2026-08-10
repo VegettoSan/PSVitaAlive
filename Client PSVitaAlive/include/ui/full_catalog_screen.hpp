decd59471036d96b005ad5cb659c4c3714e2bc77
VegettoSanDev <72358120+VegettoSan@users.noreply.github.com>
2026-08-09 21:17:44 -0500

    Expand Vita UI for images catalog loading and catalog switching

--- Client PSVitaAlive/include/ui/full_catalog_screen.hpp
+++ Client PSVitaAlive/include/ui/full_catalog_screen.hpp
@@ -1,11 +1,13 @@
 #pragma once
 
 #include "ui/ui_types.hpp"
+#include "ui/image_cache.hpp"
 
 #include <vita2d.h>
 
 #include <functional>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 namespace psvitaalive {
@@ -15,6 +17,7 @@ class FullCatalogScreen {
 public:
     using InstallRequestFn = std::function<bool(const CatalogItem&)>;
     using InstallStatusFn = std::function<std::string()>;
+    using CatalogChangeFn = std::function<bool(CatalogType)>;
 
     FullCatalogScreen();
     ~FullCatalogScreen();
@@ -23,23 +26,34 @@ public:
     void shutdown();
     bool updateAndDraw();
 
-    void setInstallCallbacks(
-        InstallRequestFn requestInstall,
-        InstallStatusFn statusText
-    );
-
+    void setInstallCallbacks(InstallRequestFn requestInstall, InstallStatusFn statusText);
+    void setCatalogChangeCallback(CatalogChangeFn callback);
+    void setImageCache(ImageCache* cache);
     void setCatalogItems(std::vector<CatalogItem> items);
+
+    void setCatalogLoading(bool loading, const std::string& label, uint64_t current, uint64_t total, const std::string& message);
+    void setCatalogError(const std::string& error);
 
 private:
     UiState state_;
     std::vector<CatalogItem> items_;
     InstallRequestFn installRequest_;
     InstallStatusFn installStatusText_;
+    CatalogChangeFn catalogChange_;
+    ImageCache* imageCache_ = nullptr;
 
     vita2d_pgf* font_ = nullptr;
     bool ready_ = false;
 
-    void loadMockData();
+    bool catalogLoading_ = false;
+    std::string catalogLoadingLabel_;
+    std::string catalogLoadingMessage_;
+    uint64_t catalogLoadingCurrent_ = 0;
+    uint64_t catalogLoadingTotal_ = 0;
+    std::string catalogError_;
+
+    std::unordered_map<std::string, vita2d_texture*> textures_;
+
     void handleInput();
     void draw();
     void drawFullCatalog();
@@ -52,6 +66,10 @@ private:
     void drawDetailPanel(int x, int y, int width, int height);
     void drawCatalogCard(const CatalogItem& item, int index, int x, int y, int width, int height, bool focused);
     void drawDetailContent(const CatalogItem& item, int x, int y, int width, int height);
+    void drawLoadingOverlay();
+    void drawImage(const std::string& url, const std::string& namespaceName, int x, int y, int width, int height);
+    void releaseTextures();
+
     void startOpeningDetail();
     void startClosingDetail();
     void updateTransition();
