22f63ffae292299c25c7955cea51a26d66e7c87e
VegettoSanDev <72358120+VegettoSan@users.noreply.github.com>
2026-08-18 23:26:06 -0500

    fix(client): kAppDir ux0:/app/PSVAS1178 for VitaDB-style self-update

--- Client PSVitaAlive/include/update/update_checker.hpp
+++ Client PSVitaAlive/include/update/update_checker.hpp
@@ -52,7 +52,7 @@ public:
     using ApplyCancelFn = std::function<bool()>;
 
     static constexpr const char* kTitleId = "PSVAS1178";
-    static constexpr const char* kAppDir = "ux0:app/PSVAS1178";
+    static constexpr const char* kAppDir = "ux0:/app/PSVAS1178";
     static constexpr const char* kUpdateDir = "ux0:data/psvitaalive/update";
     static constexpr const char* kVpkPath = "ux0:data/psvitaalive/update/PSVitaAlive.vpk";
 
